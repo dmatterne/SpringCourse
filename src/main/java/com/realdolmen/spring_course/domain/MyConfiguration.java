6807c5bf12f5da394046426f8f04df7c891945e2
David Matterne <david.matterne@gmail.com>
2016-08-26 11:27:44 +0200

    exercises

---
+++ src/main/java/com/realdolmen/spring_course/domain/MyConfiguration.java
@@ -0,0 +1,22 @@
+package com.realdolmen.spring_course.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by stannisbaratheon on 26/08/16.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.realdolmen")
+public class MyConfiguration {
+    @Bean
+    public Quest blaQuest() {
+        return new SlayDragonQuest();
+    }
+
+    @Bean
+    public Knight fooKnight() {
+        return new BraveKnight(blaQuest());
+    }
+}
