5b8f49f6a83127f7f43de04820f34086379c89a4
David Matterne <david.matterne@gmail.com>
2016-08-30 12:04:36 +0200

    Aspects

--- src/main/java/com/realdolmen/spring_course/domain/Minstrel.java
+++ src/main/java/com/realdolmen/spring_course/domain/Minstrel.java
@@ -1,10 +1,52 @@
 package com.realdolmen.spring_course.domain;
 
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by stannisbaratheon on 30/08/16.
  */
-
+@Component
+@Aspect
 public class Minstrel {
+
+    public Minstrel() {
+    }
+
+    @Pointcut("execution(* *.embarkOnQuest(..))")
+    public void embarksOnQuest() {
+
+    }
+
+    @Pointcut("execution(* *.embark(..))")
+    public void embarks() {
+
+    }
+
+    @Before("embarksOnQuest()")
+    public void singBeforeQuest() {
+        System.out.println("Is het een vliegtuig, is het een vogel, nee dat is het NIET, het is MEGA-KNIGHT die je aan de hemel ziet!");
+    }
+
+    @After("embarks()")
+    @Order(2)
+    public void singAfterQuest() {
+        System.out.println("La la la the quest has ended");
+    }
+
+    @AfterReturning("embarks()")
+    @Order(1)
+    public void singAfterQuestComplete() {
+        System.out.println("And he lived happily ever after!");
+    }
+
+    @AfterThrowing("embarks()")
+    @Order(1)
+    public void singAfterQuestFails() {
+        System.out.println("Ding Dong The Knight is dead, which old knight, the wicked Knight");
+    }
+
+
 }
--- src/main/java/com/realdolmen/spring_course/domain/SlayDragonQuest.java
+++ src/main/java/com/realdolmen/spring_course/domain/SlayDragonQuest.java
@@ -3,11 +3,19 @@ package com.realdolmen.spring_course.domain;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 @Primary
 public class SlayDragonQuest implements Quest {
     @Override
     public void embark() {
-        System.out.println("Going to slay dragon");
+        if(new Random().nextBoolean()) {
+            throw new RuntimeException("Knight gets eaten ");
+        } else {
+            System.out.println("Going to slay dragon");
+        }
+
+
     }
 }
