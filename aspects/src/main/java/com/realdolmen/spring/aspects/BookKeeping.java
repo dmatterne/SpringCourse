07654b36e1849d39f57c020db725557f488eb98a
David Matterne <david.matterne@gmail.com>
2016-08-30 13:10:55 +0200

    Aspect exercise1

--- aspects/src/main/java/com/realdolmen/spring/aspects/BookKeeping.java
+++ aspects/src/main/java/com/realdolmen/spring/aspects/BookKeeping.java
@@ -1,5 +1,6 @@
 package com.realdolmen.spring.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
@@ -8,11 +9,14 @@ import org.aspectj.lang.annotation.Pointcut;
 public class BookKeeping {
     // TODO: Add an advice that advises the Zoo.accept(Visitor) method
 
-    @Pointcut("execution ")
-    public void adviseZooAccept
+    @Pointcut("execution( boolean Zoo.accept(Visitor))")
+    public void adviseZooAccept () {
+
+    }
 
     // TODO: Make sure the advice uses the return value of the the adviced method to keep track of happy and unhappy visitors
 
+    @After(value = "")
     public int getHappyVisitorCount() {
         return 0;
     }
