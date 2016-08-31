28cb7b06c3bb154df3d72f6f1d4683ed5583872a
David Matterne <david.matterne@gmail.com>
2016-08-31 09:43:48 +0200

    adding jdbcrepos

--- data-access-jdbc/src/main/java/com/realdolmen/spring/ZooConfig.java
+++ data-access-jdbc/src/main/java/com/realdolmen/spring/ZooConfig.java
@@ -1,10 +1,16 @@
 package com.realdolmen.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ZooConfig {
     // TODO: add a datasource for the production profile. We will use BasicDataSource from Apache DBCP. Use connection properties for MySQL (url = "jdbc:mysql://localhost:3306/zoo", user = "root", password = "")
+
+    @Profile("production")
+    @Bean
+
 
     // TODO: add a JDBC template
 }
--- data-access-jdbc/src/test/java/com/realdolmen/spring/TestConfig.java
+++ data-access-jdbc/src/test/java/com/realdolmen/spring/TestConfig.java
@@ -1,5 +1,24 @@
 package com.realdolmen.spring;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
 public class TestConfig {
     // TODO: Use this class to add an embedded datasource for the test profile (you will have to set this up yourself)
+
+    @Profile("test")
+    @Bean
+    private DataSource embeddedDataSource () {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("")
+                .addScript("")
+                .build();
+
+    }
+
 }
